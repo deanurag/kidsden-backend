9ec9feb38280af014726c86671cb15881232247f
Anurag De <anuragde2004@gmail.com>
2025-10-06 13:00:46 +0530

    updated

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -1,6 +1,6 @@
 const redis = require('redis');
 const client = redis.createClient({
-    url: `redis://${process.env.REDIS_HOST || 'redis'}:${process.env.REDIS_PORT || 6379}`
+    url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`
 });
 
 client.on('error', (err) => console.error('Redis Client Error', err));
